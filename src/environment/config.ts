caba3c1193bf9fabdef442051ffdb62ae656586b
JPHV2002 <jpvellenich@gmail.com>
2023-12-12 18:23:47 -0300

    feat: adding port to env

--- src/environment/config.ts
+++ src/environment/config.ts
@@ -6,3 +6,4 @@ export const dbHost = process.env.DB_HOST || "";
 export const dbUser = process.env.DB_USER || "";
 export const dbPassword = process.env.DB_PASSWORD || "";
 export const dbDataBase = process.env.DB_DATA_BASE || "";
+export const dbPort = process.env.DB_PORT || 3306;
