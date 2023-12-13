ff404b833f39bf811c547f987da88317c800c119
JPHV2002 <jpvellenich@gmail.com>
2023-12-13 10:50:15 -0300

    feat: logs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 import express from "express";
+import * as expressWinston from "express-winston";
+
 import db from "./models/index";
 import userRoutes from "./routes/UserRoutes";
 import urlRoutes from "./routes/UrlRoutes";
 import { getShortedHandle } from "./controllers/url/getShorted/get";
+import { format, transports } from "winston";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  expressWinston.logger({
+    transports: [
+      new transports.Console(),
+      new transports.File({
+        filename: "./log/logs.log",
+      }),
+      new transports.File({
+        level: "warn",
+        filename: "./log/warnLogs.log",
+      }),
+      new transports.File({
+        level: "error",
+        filename: "./log/errorLogs.log",
+      }),
+    ],
+    format: format.combine(
+      format.json(),
+      format.timestamp(),
+      format.prettyPrint()
+    ),
+    statusLevels: true,
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/url", urlRoutes);
